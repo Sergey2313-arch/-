5b82141d30806a884b377b6fab555d2794a85257
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-23 21:56:08 +0300

    Add user roles

---
+++ src/ReviMarket.Web/Models/UserRoles.cs
@@ -0,0 +1,8 @@
+namespace ReviMarket.Web.Models;
+
+public static class UserRoles
+{
+    public const string Customer = "Customer";
+    public const string Creator = "Creator";
+    public const string Admin = "Admin";
+}
