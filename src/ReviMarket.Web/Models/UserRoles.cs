1f9e987841370b24b375e38564696e1c4aeb2271
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-23 23:44:18 +0300

    Expand platform roles

--- src/ReviMarket.Web/Models/UserRoles.cs
+++ src/ReviMarket.Web/Models/UserRoles.cs
@@ -6,4 +6,22 @@ public static class UserRoles
     public const string Creator = "Creator";
     public const string Admin = "Admin";
     public const string Manager = "Manager";
+    public const string Owner = "Owner";
+    public const string CoOwner = "CoOwner";
+    public const string Moderator = "Moderator";
+    public const string Lawyer = "Lawyer";
+    public const string SupportAgent = "SupportAgent";
+
+    public static readonly string[] All =
+    {
+        Customer,
+        Creator,
+        Admin,
+        Manager,
+        Owner,
+        CoOwner,
+        Moderator,
+        Lawyer,
+        SupportAgent
+    };
 }
