8892313b533289f9ea6ca544b522296915092320
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-23 23:46:11 +0300

    Add role assignment actions

--- src/ReviMarket.Web/Controllers/TeamController.cs
+++ src/ReviMarket.Web/Controllers/TeamController.cs
@@ -6,7 +6,7 @@ using ReviMarket.Web.Models;
 
 namespace ReviMarket.Web.Controllers;
 
-[Authorize(Roles = UserRoles.Admin + "," + UserRoles.Manager)]
+[Authorize(Roles = UserRoles.Admin + "," + UserRoles.Manager + "," + UserRoles.Owner + "," + UserRoles.CoOwner)]
 public class TeamController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
@@ -19,6 +19,35 @@ public class TeamController : Controller
     public async Task<IActionResult> Index()
     {
         var users = await _userManager.Users.OrderByDescending(x => x.CreatedAt).ToListAsync();
+        ViewBag.Roles = UserRoles.All;
         return View(users);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> SetRole(string userId, string role)
+    {
+        if (!UserRoles.All.Contains(role)) return BadRequest();
+
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user is null) return NotFound();
+
+        var currentRoles = await _userManager.GetRolesAsync(user);
+        var rolesToRemove = currentRoles.Where(x => UserRoles.All.Contains(x)).ToList();
+
+        if (rolesToRemove.Any())
+        {
+            await _userManager.RemoveFromRolesAsync(user, rolesToRemove);
+        }
+
+        if (!await _userManager.IsInRoleAsync(user, role))
+        {
+            await _userManager.AddToRoleAsync(user, role);
+        }
+
+        user.AccountType = role;
+        await _userManager.UpdateAsync(user);
+
+        return RedirectToAction(nameof(Index));
     }
 }
