f71e19aac42379298734e0f1db65cc5b24ee5dd4
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-23 23:45:13 +0300

    Add support and case tables

--- src/ReviMarket.Web/Data/ApplicationDbContext.cs
+++ src/ReviMarket.Web/Data/ApplicationDbContext.cs
@@ -12,6 +12,9 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 
     public DbSet<MarketItem> MarketItems => Set<MarketItem>();
     public DbSet<ChatMessage> ChatMessages => Set<ChatMessage>();
+    public DbSet<SupportRequest> SupportRequests => Set<SupportRequest>();
+    public DbSet<UserCase> UserCases => Set<UserCase>();
+    public DbSet<OrderCase> OrderCases => Set<OrderCase>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -34,5 +37,53 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
             .WithMany()
             .HasForeignKey(x => x.ReceiverId)
             .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<SupportRequest>()
+            .HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Entity<SupportRequest>()
+            .HasOne(x => x.Agent)
+            .WithMany()
+            .HasForeignKey(x => x.AgentId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Entity<UserCase>()
+            .HasOne(x => x.CreatedBy)
+            .WithMany()
+            .HasForeignKey(x => x.CreatedById)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Entity<UserCase>()
+            .HasOne(x => x.TargetUser)
+            .WithMany()
+            .HasForeignKey(x => x.TargetUserId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Entity<UserCase>()
+            .HasOne(x => x.Agent)
+            .WithMany()
+            .HasForeignKey(x => x.AgentId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Entity<OrderCase>()
+            .HasOne(x => x.MarketItem)
+            .WithMany()
+            .HasForeignKey(x => x.MarketItemId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Entity<OrderCase>()
+            .HasOne(x => x.CreatedBy)
+            .WithMany()
+            .HasForeignKey(x => x.CreatedById)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Entity<OrderCase>()
+            .HasOne(x => x.Agent)
+            .WithMany()
+            .HasForeignKey(x => x.AgentId)
+            .OnDelete(DeleteBehavior.SetNull);
     }
 }
