92be81843a8480d5bd15196eafda6c3fc09fdcfd
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-23 22:56:07 +0300

    Add manager role

--- src/ReviMarket.Web/Models/UserRoles.cs
+++ src/ReviMarket.Web/Models/UserRoles.cs
@@ -5,4 +5,5 @@ public static class UserRoles
     public const string Customer = "Customer";
     public const string Creator = "Creator";
     public const string Admin = "Admin";
+    public const string Manager = "Manager";
 }
