566d592979644ae8b03c001f27cf0472deb830d6
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-25 17:50:46 +0300

    Register deal finance tables

--- src/ReviMarket.Web/Data/ApplicationDbContext.cs
+++ src/ReviMarket.Web/Data/ApplicationDbContext.cs
@@ -18,93 +18,32 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Wallet> Wallets => Set<Wallet>();
     public DbSet<PaymentTransaction> PaymentTransactions => Set<PaymentTransaction>();
     public DbSet<PaymentInvoice> PaymentInvoices => Set<PaymentInvoice>();
+    public DbSet<Deal> Deals => Set<Deal>();
+    public DbSet<WithdrawalRequest> WithdrawalRequests => Set<WithdrawalRequest>();
+    public DbSet<PlatformTransaction> PlatformTransactions => Set<PlatformTransaction>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<MarketItem>()
-            .HasOne(x => x.Owner)
-            .WithMany()
-            .HasForeignKey(x => x.OwnerId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<ChatMessage>()
-            .HasOne(x => x.Sender)
-            .WithMany()
-            .HasForeignKey(x => x.SenderId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Entity<ChatMessage>()
-            .HasOne(x => x.Receiver)
-            .WithMany()
-            .HasForeignKey(x => x.ReceiverId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Entity<SupportRequest>()
-            .HasOne(x => x.User)
-            .WithMany()
-            .HasForeignKey(x => x.UserId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<SupportRequest>()
-            .HasOne(x => x.Agent)
-            .WithMany()
-            .HasForeignKey(x => x.AgentId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<UserCase>()
-            .HasOne(x => x.CreatedBy)
-            .WithMany()
-            .HasForeignKey(x => x.CreatedById)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<UserCase>()
-            .HasOne(x => x.TargetUser)
-            .WithMany()
-            .HasForeignKey(x => x.TargetUserId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<UserCase>()
-            .HasOne(x => x.Agent)
-            .WithMany()
-            .HasForeignKey(x => x.AgentId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<OrderCase>()
-            .HasOne(x => x.MarketItem)
-            .WithMany()
-            .HasForeignKey(x => x.MarketItemId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<OrderCase>()
-            .HasOne(x => x.CreatedBy)
-            .WithMany()
-            .HasForeignKey(x => x.CreatedById)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<OrderCase>()
-            .HasOne(x => x.Agent)
-            .WithMany()
-            .HasForeignKey(x => x.AgentId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.Entity<Wallet>()
-            .HasOne(x => x.User)
-            .WithMany()
-            .HasForeignKey(x => x.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        builder.Entity<PaymentTransaction>()
-            .HasOne(x => x.User)
-            .WithMany()
-            .HasForeignKey(x => x.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        builder.Entity<PaymentInvoice>()
-            .HasOne(x => x.User)
-            .WithMany()
-            .HasForeignKey(x => x.UserId)
-            .OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<MarketItem>().HasOne(x => x.Owner).WithMany().HasForeignKey(x => x.OwnerId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<ChatMessage>().HasOne(x => x.Sender).WithMany().HasForeignKey(x => x.SenderId).OnDelete(DeleteBehavior.Restrict);
+        builder.Entity<ChatMessage>().HasOne(x => x.Receiver).WithMany().HasForeignKey(x => x.ReceiverId).OnDelete(DeleteBehavior.Restrict);
+        builder.Entity<SupportRequest>().HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<SupportRequest>().HasOne(x => x.Agent).WithMany().HasForeignKey(x => x.AgentId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<UserCase>().HasOne(x => x.CreatedBy).WithMany().HasForeignKey(x => x.CreatedById).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<UserCase>().HasOne(x => x.TargetUser).WithMany().HasForeignKey(x => x.TargetUserId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<UserCase>().HasOne(x => x.Agent).WithMany().HasForeignKey(x => x.AgentId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<OrderCase>().HasOne(x => x.MarketItem).WithMany().HasForeignKey(x => x.MarketItemId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<OrderCase>().HasOne(x => x.CreatedBy).WithMany().HasForeignKey(x => x.CreatedById).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<OrderCase>().HasOne(x => x.Agent).WithMany().HasForeignKey(x => x.AgentId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<Wallet>().HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<PaymentTransaction>().HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<PaymentInvoice>().HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<Deal>().HasOne(x => x.Customer).WithMany().HasForeignKey(x => x.CustomerId).OnDelete(DeleteBehavior.Restrict);
+        builder.Entity<Deal>().HasOne(x => x.Executor).WithMany().HasForeignKey(x => x.ExecutorId).OnDelete(DeleteBehavior.Restrict);
+        builder.Entity<Deal>().HasOne(x => x.MarketItem).WithMany().HasForeignKey(x => x.MarketItemId).OnDelete(DeleteBehavior.SetNull);
+        builder.Entity<WithdrawalRequest>().HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<PlatformTransaction>().HasOne(x => x.Deal).WithMany().HasForeignKey(x => x.DealId).OnDelete(DeleteBehavior.SetNull);
     }
 }
