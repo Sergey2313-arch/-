4085bb440ca4e4baa34eceffc3310a3eb4e38b7b
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-25 17:53:00 +0300

    Add compact finance admin controller

---
+++ src/ReviMarket.Web/Controllers/AdminFinanceController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReviMarket.Web.Data;
+using ReviMarket.Web.Models;
+
+namespace ReviMarket.Web.Controllers;
+
+[Authorize(Roles = UserRoles.Admin + "," + UserRoles.Manager + "," + UserRoles.Owner + "," + UserRoles.CoOwner)]
+public class AdminFinanceController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public AdminFinanceController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        ViewBag.TotalDeals = await _db.Deals.SumAsync(x => x.Amount);
+        ViewBag.TotalCommission = await _db.PlatformTransactions.SumAsync(x => x.Amount);
+        ViewBag.TotalHold = await _db.Wallets.SumAsync(x => x.HoldBalance);
+        ViewBag.TotalBalance = await _db.Wallets.SumAsync(x => x.Balance);
+        ViewBag.Withdrawals = await _db.WithdrawalRequests.Include(x => x.User).OrderByDescending(x => x.CreatedAt).Take(20).ToListAsync();
+        ViewBag.Deals = await _db.Deals.Include(x => x.Customer).Include(x => x.Executor).OrderByDescending(x => x.CreatedAt).Take(20).ToListAsync();
+        return View();
+    }
+}
